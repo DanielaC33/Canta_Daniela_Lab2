150a6a10746dade06718f5bacaafe35c5aeeca10
DANIELA-ALEXANDRA CANTA <daniela.canta@stud.ubbcluj.ro>
2024-10-16 17:57:54 +0300

    Add project files.

---
+++ Data/Canta_Daniela_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Canta_Daniela_Lab2.Models;
+
+namespace Canta_Daniela_Lab2.Data
+{
+    public class Canta_Daniela_Lab2Context : DbContext
+    {
+        public Canta_Daniela_Lab2Context (DbContextOptions<Canta_Daniela_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Canta_Daniela_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Canta_Daniela_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Canta_Daniela_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Migrations/20241016132739_AfterAuthors.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canta_Daniela_Lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AfterAuthors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
